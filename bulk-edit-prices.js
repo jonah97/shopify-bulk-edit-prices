bd2f3fb13c7a812a339881f5ef3aa671523b73f9
Jonah Rusch <admin@Jonahs-MBP.fritz.box>
2021-05-20 22:19:41 +0200

    add price transformation

--- bulk-edit-prices.js
+++ bulk-edit-prices.js
@@ -7,28 +7,48 @@ const options = {
     url: process.env.SHOPIFY_ACCESS_URL_PRODUCTS
 }
 
-request.get(options, (err, response, body) => {
-    if (!err && response.statusCode == 200) {
-        let data = JSON.parse(body)
+let raisedAmount = 100
 
-        let products = []
+function getProducts() {
+    return new Promise((resolve, reject) => {
+        request.get(options, (err, response, body) => {
+            if (!err && response.statusCode == 200) {
+                let data = JSON.parse(body)
 
-        class Product {
-            constructor(id, variants) {
-                this.id = id
-                this.variants = variants
-                this.prices = []
+                let products = []
+
+                class Product {
+                    constructor(id, variants) {
+                        this.id = id
+                        this.variants = variants
+                        this.prices = []
+                    }
+                }
+
+                for (let i = 0; i < data.products.length; i++) {
+                    products.push(new Product(data.products[i].id, data.products[i].variants.length))
+
+                    for (let j = 0; j < data.products[i].variants.length; j++) {
+                        products[i].prices.push(parseFloat(data.products[i].variants[j].price))
+                    }
+                }
+                resolve(products)
+            } else {
+                reject(err)
             }
+        })
+    })
+}
+
+getProducts().catch((err) => {
+    console.error(err)
+}).then((products) => {
+
+    for (let i = 0; i < products.length; i++) {
+        for (let j = 0; j < products[i].prices.length; j++) {
+            products[i].prices[j] += raisedAmount
         }
-
-        for (let i = 0; i < data.products.length; i++) {
-            products.push(new Product(data.products[i].id, data.products[i].variants.length))
-
-            for (let j = 0; j < data.products[i].variants.length; j++) {
-                products[i].prices.push(data.products[i].variants[j].price)
-            }
-        }
-
-        console.log(products)
     }
+
+    console.log(products)
 })
